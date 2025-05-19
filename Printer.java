6ae6a90f8fc7382d9851383d3cc86dad799f52f5
loveAI0 <140394889+loveAI0@users.noreply.github.com>
2025-05-19 08:31:08 +0000

    TextBlock is used.

---
+++ Printer.java
@@ -0,0 +1,13 @@
+class Printer{
+    public static void main(String[] args) {
+       String printerText = """ 
+            I love animals!
+            Let's check on the animals...
+            The deer looks fine.
+            The bat looks happy.
+            The lion looks healthy.       
+        """; 
+        System.out.println(printerText);
+    }
+    
+}
