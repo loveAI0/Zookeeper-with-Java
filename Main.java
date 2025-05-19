e1e427dc05250d3acb8c3b68b7c4d89b80075a06
loveAI0 <140394889+loveAI0@users.noreply.github.com>
2025-05-19 08:36:08 +0000

    animal Printer is created

--- Main.java
+++ Main.java
@@ -1,3 +1,7 @@
 class Main {
-    
+    public static void main(String[] args) {
+        Printer.textPrinter();
+        System.out.println();
+        Printer.animalPrinter();
+    }
 }
--- Printer.java
+++ Printer.java
@@ -1,13 +1,37 @@
 class Printer{
-    public static void main(String[] args) {
-       String printerText = """ 
+    
+    static void textPrinter(){
+        String printerText = """ 
             I love animals!
             Let's check on the animals...
             The deer looks fine.
             The bat looks happy.
-            The lion looks healthy.       
-        """; 
+            The lion looks healthy."""; 
         System.out.println(printerText);
+    }
+
+    static void animalPrinter(){
+        String camel = """
+            Switching on the camera in the camel habitat...
+             ___.-''''-.
+            /___  @    |
+            ',,,,.     |         _.'''''''._
+                 '     |        /           \\
+                 |     \\    _.-'             \\
+                 |      '.-'                  '-.
+                 |                               ',
+                 |                                '',
+                  ',,-,                           ':;
+                       ',,| ;,,                 ,' ;;
+                          ! ; !'',,,',',,,,'!  ;   ;:
+                         : ;  ! !       ! ! ;  ;   :;
+                         ; ;   ! !      ! !  ; ;   ;,
+                        ; ;    ! !     ! !   ; ;
+                        ; ;    ! !    ! !     ; ;
+                       ;,,      !,!   !,!     ;,;
+                       /_I      L_I   L_I     /_I
+            Look at that! Our little camel is sunbathing!""";
+            System.out.println(camel);
     }
     
 }
