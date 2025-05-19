b06ba827f5ffaa7fa582f528499dc728a91ec36b
loveAI0 <140394889+loveAI0@users.noreply.github.com>
2025-05-19 08:53:43 +0000

    Camera is installed and working.

--- Main.java
+++ Main.java
@@ -1,7 +1,14 @@
+import java.util.Scanner;
 class Main {
     public static void main(String[] args) {
         Printer.textPrinter();
         System.out.println();
-        Printer.animalPrinter();
+
+        System.out.println("Please enter the number of the habitat you would like to view:");
+
+        Scanner input = new Scanner(System.in);
+        int cameraNumber = input.nextInt();
+
+        Printer.animalPrinter(cameraNumber);
     }
 }
--- Printer.java
+++ Printer.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.LinkedHashSet;
+import java.util.List;
 class Printer{
     
     static void textPrinter(){
@@ -10,7 +13,7 @@ class Printer{
         System.out.println(printerText);
     }
 
-    static void animalPrinter(){
+    static void animalPrinter(int cameraNumber){
         String camel = """
             Switching on the camera in the camel habitat...
              ___.-''''-.
@@ -31,7 +34,128 @@ class Printer{
                        ;,,      !,!   !,!     ;,;
                        /_I      L_I   L_I     /_I
             Look at that! Our little camel is sunbathing!""";
-            System.out.println(camel);
+        
+        String lion = """
+            Switching on the camera in the lion habitat...
+                                                           ,w.
+                                                         ,YWMMw  ,M  ,
+                                    _.---.._   __..---._.'MMMMMw,wMWmW,
+                               _.-""        '''           YP"WMMMMMMMMMb,
+                            .-' __.'                   .'     MMMMW^WMMMM;
+                _,        .'.-'"; `,       /`     .--""      :MMM[==MWMW^;
+             ,mM^"     ,-'.'   /   ;      ;      /   ,       MMMMb_wMW"  @\\
+            ,MM:.    .'.-'   .'     ;     `\\    ;     `,     MMMMMMMW `"=./`-,
+            WMMm__,-'.'     /      _.\\      F'''-+,,   ;_,_.dMMMMMMMM[,_ / `=_}
+            "^MP__.-'    ,-' _.--""   `-,   ;       \\  ; ;MMMMMMMMMMW^``; __|
+                       /   .'            ; ;         )  )`{  \\ `"^W^`,   \\  :
+                      /  .'             /  (       .'  /     Ww._     `.  `"
+                     /  Y,              `,  `-,=,_{   ;      MMMP`""-,  `-._.-,
+                    (--, )                `,_ / `) \\/"")      ^"      `-, -;"\\:
+            The lion is roaring!""";
+
+        String deer = """
+            Switching on the camera in the deer habitat...
+               /|       |\\
+            `__\\       //__'
+               ||      ||
+             \\__`\\     |'__/
+               `_\\   //_'
+               _.,:---;,._
+               \\_:     :_/
+                 |@. .@|
+                 |     |
+                 ,\\.-./ \\
+                 ;;`-'   `---__________-----.-.
+                 ;;;                         \\_\\
+                 ';;;                         |
+                  ;    |                      ;
+                   \\   \\     \\        |      /
+                    \\_, \\    /        \\     |\\
+                      |';|  |,,,,,,,,/ \\    \\ \\_
+                      |  |  |           \\   /   |
+                      \\  \\  |           |  / \\  |
+                       | || |           | |   | |
+                       | || |           | |   | |
+                       | || |           | |   | |
+                       |_||_|           |_|   |_|
+                      /_//_/           /_/   /_/
+            Our 'Bambi' looks hungry. Let's go to feed it!""";
+
+        String goose = """
+            Switching on the camera in the goose habitat...
+            
+                                                _
+                                            ,-"" "".
+                                          ,'  ____  `.
+                                        ,'  ,'    `.  `._
+               (`.         _..--.._   ,'  ,'        \\    \\
+              (`-.\\    .-""        ""'   /          (  d _b
+             (`._  `-"" ,._             (            `-(   \\
+             <_  `     (  <`<            \\              `-._\\
+              <`-       (__< <           :
+               (__        (_<_<          ;
+                `------------------------------------------
+            The goose is staring intently at you... Maybe it's time to change the channel?""";
+
+        String bat = """
+            Switching on the camera in the bat habitat...
+            _________________               _________________
+             ~-.              \\  |\\___/|  /              .-~
+                 ~-.           \\ / o o \\ /           .-~
+                    >           \\  W  //           <
+                   /             /~---~\\             \\
+                  /_            |       |            _\\
+                     ~-.        |       |        .-~
+                        ;        \\     /        i
+                       /___      /\\   /\\      ___\\
+                            ~-. /  \\_/  \\ .-~
+                               V         V
+            This bat looks like it's doing fine.""";
+
+        String rabbit = """
+            Switching on the camera in the rabbit habitat...
+                     ,
+                    /|      __
+                   / |   ,-~ /
+                  Y :|  //  /
+                  | jj /( .^
+                  >-"~"-v"
+                 /       Y
+                jo  o    |
+               ( ~T~     j
+                >._-' _./
+               /   "~"  |
+              Y     _,  |
+             /| ;-"~ _  l
+            / l/ ,-"~    \\
+            \\//\\/      .- \\
+             Y        /    Y
+             l       I     !
+             ]\\      _\\    /"\\
+            (" ~----( ~   Y.  )
+            It looks like we will soon have more rabbits!""";
+
+            LinkedHashSet <String> animalsCamera = new LinkedHashSet<>();
+            animalsCamera.add(camel);
+            animalsCamera.add(lion);
+            animalsCamera.add(deer);
+            animalsCamera.add(goose);
+            animalsCamera.add(bat);
+            animalsCamera.add(rabbit);
+
+            List<String> listOfCamera = new ArrayList<>(new LinkedHashSet<>(animalsCamera));
+            
+            if(cameraNumber > listOfCamera.size()){
+                System.out.println("Enter Valid Camera Number.");
+            }
+            else{
+                System.out.println(listOfCamera.get(cameraNumber));
+                String endMsg = """
+                        ---
+                        You've reached the end of the program. To check another habitat, please restart the watcher.""";
+                System.out.println(endMsg);
+            } 
+
     }
     
 }
